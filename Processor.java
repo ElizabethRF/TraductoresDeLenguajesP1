0e6591bb829d232f1a622bda710706be105538dc
Elizabeth Rodriguez Fallas <elizabeth@MBPEli.local>
2019-03-08 11:43:51 +0100

    work for both cases

--- Processor.java
+++ Processor.java
@@ -54,8 +54,7 @@ public class Processor{
             System.out.println(" i para substring " + i);
             String subWord = word.substring(i);
             System.out.println(" subword: "+ subWord );
-            System.out.println(" subwordLength: "+ subWord.length());
-            // Ahora lo voy a cambiar al longest path del primer estado final, pero debe ser para cada uno 
+            System.out.println(" subwordLength: "+ subWord.length()); 
             getOutputDegree(); // calcular el output degree para evaluar abajo
             int[] longestPath = getLongestPath(m.getFinalState().get(0), subWord, subWord.length() - 1);
             j -= longestPath[1];
@@ -82,6 +81,13 @@ public class Processor{
         System.out.println(" i " + i);
 
         Stack<Integer> statesRight = m.getRight(rightState, word.charAt(i));
+
+        System.out.println(" states right"); 
+        for(int a = 0; a< statesRight.size(); a++){
+            System.out.println(statesRight.get(0)); 
+        }
+        System.out.println(" end states right"); 
+
         int[] actualPath = new int[2];
         actualPath[0] = rightState;
         actualPath[1] = 0;
@@ -95,13 +101,13 @@ public class Processor{
             int state = statesRight.pop();
             if(m.getFinalState().indexOf(state) == -1){
                 if(degrees.get(state) <= 1){
-                    
-                
+
                     if(i == 0){
                         maxPath[0] = state; 
                         maxPath[1] = 1;
                     }else{
                         actualPath = getLongestPath(state, word, i - 1);
+                        System.out.println("recursivooo");
                         actualPath[1]++;
                         if(actualPath[1] > maxPath[1]){
                             maxPath[0] = actualPath[0];
@@ -126,16 +132,33 @@ public class Processor{
         System.out.println(m.toString());
     }
 
-    public void getOutputDegree(){
-        ArrayList<ArrayList<String>> matrix = m.getMatrix(); 
-        for(int i = 0; i< matrix.size() ; i++){
+    public void createDegrees(ArrayList<ArrayList<String>> matrix){
+        degrees = new ArrayList<Integer>(); 
+        for(int i = 0 ; i< matrix.size() ; i++){
             degrees.add(0); 
+        }
+    }
+
+    public void getOutputDegree(){
+        ArrayList<ArrayList<String>> matrix = m.getMatrix();
+        int totalOut = 0;  
+        createDegrees(matrix); // create all degrees needed 
+        for(int i = 0 ; i< matrix.size() ; i++){
+            totalOut = 0; 
             for(int j = 0; j< matrix.size(); j++){
                 if(matrix.get(i).get(j) != ""){
-                    degrees.set(i,degrees.get(i) + 1);
+                    totalOut += 1; 
                 }
             }
+            degrees.set(i,totalOut);
 
         }
+        //imprimir grados 
+        System.out.println(" GRADOS");
+        for(int i = 0; i< degrees.size(); i++){
+            System.out.println(" grados para " + i + " : "+  degrees.get(i));
+        }
+        System.out.println("FINGRADOS\n ");
+
     }
 }
